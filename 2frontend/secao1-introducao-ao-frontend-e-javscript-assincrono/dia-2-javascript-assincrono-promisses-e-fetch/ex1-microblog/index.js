74da4f3fb57b8a9bcc654a5e7f744aa7fc13da31
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-01-11 20:11:46 -0300

    Fill user names Faça um fetch para a API https://dummyjson.com/users para recuperar as informações das pessoas usuárias. Depois de receber os dados, passe o array retornado pela API com as informações das pessoas usuárias para a função fillUsersSelect para que o select da página seja atualizado.

--- 2frontend/secao1-introducao-ao-frontend-e-javscript-assincrono/dia-2-javascript-assincrono-promisses-e-fetch/ex1-microblog/index.js
+++ 2frontend/secao1-introducao-ao-frontend-e-javscript-assincrono/dia-2-javascript-assincrono-promisses-e-fetch/ex1-microblog/index.js
@@ -1,20 +1,29 @@
 import './style.css';
 
 import {
-  fillUsersSelect,
-  fillPosts,
-  fillFeaturedPostComments,
-  clearPageData,
-  fillErrorMessage,
+    fillUsersSelect,
+    fillPosts,
+    fillFeaturedPostComments,
+    clearPageData,
+    fillErrorMessage,
 } from './utils/updateUI';
 
 const usersSelect = document.querySelector('#users-select');
 
 const USERS_API = 'https://dummyjson.com/users';
-// faça a lógica para pegar as informações das pessoas usuárias e preencher o select aqui.
+fetch(USERS_API)
+    .then(result => result.json())
+    .then(json => {
+        fillUsersSelect(json.users)
+        // console.log(json);
+        return json;
+    })
+    .catch(error => console.log(`Erro: ${error}`))
+// .finally(() => {console.log('Saindo!!')})
+;
 
 usersSelect.addEventListener('change', () => {
-  clearPageData();
+    clearPageData();
 
-  // faça a lógica para pegar as informações dos posts da pessoa selecionada e dos comentários do post destacado aqui.
+    // faça a lógica para pegar as informações dos posts da pessoa selecionada e dos comentários do post destacado aqui.
 });
