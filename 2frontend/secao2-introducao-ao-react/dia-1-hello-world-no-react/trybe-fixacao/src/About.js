20d33022bbf715e9b620d4367b5a4510adaae346
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-01-20 18:31:06 -0300

    Exercicio de fixacao React

---
+++ 2frontend/secao2-introducao-ao-react/dia-1-hello-world-no-react/trybe-fixacao/src/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class About extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Leoanrdo</h1>
+                <p>Sou uma pessoa! Um ser humano.</p>
+                <h2>Minas Habilidades</h2>
+                <ul>
+                    <li>Dormir muito</li>
+                </ul>
+            </>);
+    }
+}
+
+export default About;
