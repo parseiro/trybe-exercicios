b8ee7f0a0bba0c2ba350bd9ba7f121536ef91ff7
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-01-23 15:02:24 -0300

    First React component

---
+++ 2frontend/secao2-introducao-ao-react/dia-2-components-react/exercise-image-component/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Image from "./Image";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Learn React
+      </header>
+      <Image alternativeText="gato de botas" source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"/>
+    </div>
+  );
+}
+
+export default App;
