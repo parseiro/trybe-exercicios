c678bfd5fab4eda1655d3f499555bb3b86b361a8
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-03-02 22:35:20 -0300

    Front-end 7.1 Ex1

--- 2frontend/secao7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/exercicio1/src/App.tsx
+++ 2frontend/secao7-gerenciamento-de-estado-com-redux/dia-1-introducao-ao-redux/exercicio1/src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+const INITIAL_STATE = {
+  colors: ['white', 'black', 'red', 'green', 'blue', 'yellow'],
+  index: 0,
+};
+
+const PROXIMA_COR = 'PROXIMA COR';
+const ANTERIOR_COR = 'ANTERIOR COR';
+
+const redutor = (state = INITIAL_STATE, acao) => {
+  const { type } = acao;
+  const { colors, index: currentIndex } = state;
+  let newIndex;
+  switch (type) {
+    case PROXIMA_COR:
+      newIndex = (currentIndex + 1) % colors.length;
+      break;
+    case ANTERIOR_COR:
+      newIndex = (currentIndex - 1) % colors.length;
+      break;
+    default:
+      return state;
+  }
+  // console.log(`Index atual: ${currentIndex}, novo index: ${newIndex}`);
+  return {
+    ...state,
+    index: newIndex,
+  };
+};
+
+const loja = createStore(redutor, composeWithDevTools());
+
+const valor = document.getElementById('value');
+loja.subscribe(() => {
+  const { colors, index } = loja.getState();
+  console.log(`Novo index: ${index}, Nova cor: ${colors[index]}`);
+  if (valor) {
+    valor.innerText = colors[index];
+  }
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div id="container">
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Color: <span id="value">white</span>
         </p>
+        <button id="previous" onClick={() => loja.dispatch({ type: ANTERIOR_COR })}>
+          Previous color
+        </button>
+        <button id="next" onClick={() => loja.dispatch({ type: PROXIMA_COR })}>
+          Next color
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
