a8fa351b69abbfa7ce2e67a5eb0a3ac37cbe564a
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-03-07 18:28:33 -0300

    Front-end 7.2

---
+++ 2frontend/secao7-gerenciamento-de-estado-com-redux/dia-2-usando-redux-no-react/src/App.jsx
@@ -0,0 +1,54 @@
+import './App.css';
+import {connect} from 'react-redux';
+
+const actionCreator = (increment = 1) => ({
+    type: 'INCREMENT',
+    payload: increment,
+});
+
+function App({countState, dispatch}) {
+    return (
+        <div className="flex container items-center justify-center">
+            <button type="button" className="botao" onClick={() => dispatch(actionCreator(-1))}>
+                <svg
+                    aria-hidden="true"
+                    className="w-4 h-4"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5 10C5 9.73478 5.10536 9.48043 5.29289 9.29289C5.48043 9.10536 5.73478 9 6 9H14C14.2652 9 14.5196 9.10536 14.7071 9.29289C14.8946 9.48043 15 9.73478 15 10C15 10.2652 14.8946 10.5196 14.7071 10.7071C14.5196 10.8946 14.2652 11 14 11H6C5.73478 11 5.48043 10.8946 5.29289 10.7071C5.10536 10.5196 5 10.2652 5 10Z"
+                    />
+                </svg>
+                <span className="sr-only">Subtract</span>
+            </button>
+            {countState}
+            <button type="button" className="botao" onClick={() => dispatch(actionCreator(1))}>
+                <svg
+                    aria-hidden="true"
+                    className="w-4 h-4"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M10 5C10.2652 5 10.5196 5.10536 10.7071 5.29289C10.8946 5.48043 11 5.73478 11 6V9H14C14.2652 9 14.5196 9.10536 14.7071 9.29289C14.8946 9.48043 15 9.73478 15 10C15 10.2652 14.8946 10.5196 14.7071 10.7071C14.5196 10.8946 14.2652 11 14 11H11V14C11 14.2652 10.8946 14.5196 10.7071 14.7071C10.5196 14.8946 10.2652 15 10 15C9.73478 15 9.48043 14.8946 9.29289 14.7071C9.10536 14.5196 9 14.2652 9 14V11H6C5.73478 11 5.48043 10.8946 5.29289 10.7071C5.10536 10.5196 5 10.2652 5 10C5 9.73478 5.10536 9.48043 5.29289 9.29289C5.48043 9.10536 5.73478 9 6 9H9V6C9 5.73478 9.10536 5.48043 9.29289 5.29289C9.48043 5.10536 9.73478 5 10 5V5Z"
+                        fill="#fff"
+                    />
+                </svg>
+                <span className="sr-only">Add</span>
+            </button>
+        </div>
+    );
+}
+
+const mapStateToProps = (state) => {
+    return {countState: state.count};
+};
+
+export default connect(mapStateToProps)(App);
