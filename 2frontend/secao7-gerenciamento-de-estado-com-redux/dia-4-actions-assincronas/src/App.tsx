35e5b67818c4e3e28de4c892f6fdf0f9c0767b8d
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-03-08 17:03:06 -0300

    Exercicio 7.4 changes

--- 2frontend/secao7-gerenciamento-de-estado-com-redux/dia-4-actions-assincronas/src/App.tsx
+++ 2frontend/secao7-gerenciamento-de-estado-com-redux/dia-4-actions-assincronas/src/App.tsx
@@ -1,30 +1,22 @@
-import { useState } from "react";
 import "./App.css";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import {Button} from "flowbite-react";
-import {fetchDogImage, REQUEST_STARTED, REQUEST_SUCCESS} from "./redux/actions";
+import {fetchDogImage} from "./redux/actions";
+import * as AspectRatio from "@radix-ui/react-aspect-ratio";
 
 function App({isLoading, url, dispatch}) {
-  let botao;
-  if (!isLoading) {
-    botao = <Button color="dark" onClick={() => { dispatch(fetchDogImage());}}>
-      Random dog
-    </Button>;
-  } else {
-    botao = <></>;
-  }
 
-  let figura;
-  if (isLoading) figura =
-      <div role="status" className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center">
-        <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 dark:bg-gray-700">
-          <svg className="w-12 h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
-               fill="currentColor" viewBox="0 0 640 512">
-            <path
-                d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"/>
-          </svg>
-        </div>
-{/*        <div className="w-full">
+    let figura;
+    if (isLoading) figura =
+        <div role="status" className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center w-full h-full">
+            <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded dark:bg-gray-700">
+                <svg className="w-12 h-12 text-gray-200" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
+                     fill="currentColor" viewBox="0 0 640 512">
+                    <path
+                        d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"/>
+                </svg>
+            </div>
+            {/*        <div className="w-full">
           <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
           <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[480px] mb-2.5"></div>
           <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
@@ -32,28 +24,41 @@ function App({isLoading, url, dispatch}) {
           <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[460px] mb-2.5"></div>
           <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
         </div>*/}
-        <span className="sr-only">Loading...</span>
-      </div>;
-  else figura = <figure className="max-w-lg">
-    <img
-        className="h-auto max-w-full rounded-lg"
-        src={url}
-        alt="image description"/>
-    <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Random dog</figcaption>
-  </figure>;
+            <span className="sr-only">Loading...</span>
+        </div>;
+    else figura = (
+        <img
+            className="object-cover rounded-lg max-w-full h-full"
+            src={url}
+            alt="image description"/>
+    );
 
-  return (
-    <div id="App" className="flex flex-col items-center justify-center gap-y-10">
-      {botao}
+    return (
+        <div id="App" className="flex flex-col items-center justify-center gap-y-10">
+            <Button
+                disabled={isLoading}
+                color="dark"
+                onClick={() => {
+                    dispatch(fetchDogImage());
+                }}>
+                Random dog
+            </Button>
 
-      {figura}
-    </div>
-  )
+            <figure className="w-full">
+                <AspectRatio.Root ratio={16 / 9} className="flex flex-col items-center justify-center border">
+                    {figura}
+                </AspectRatio.Root>
+                <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">Random dog
+                </figcaption>
+            </figure>
+
+        </div>
+    )
 }
 
 const mapStateToProps = ({isFetching: isLoading, imageURL: url}) => ({
-  isLoading,
-  url,
+    isLoading,
+    url,
 });
 
 export default connect(mapStateToProps)(App);
